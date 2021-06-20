6c785846422390ff00b3bb056a3a044f951dadbc
GunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-06-20 18:35:37 +0300

    Added two forms, routes and templates for them

--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
 from flask import Flask
 
 from web.base.routes import base
+from web.users.routes import users
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = \
+    '176cec908f34145d2befd36987ad19b'
+
 app.register_blueprint(base)
+app.register_blueprint(users)
 
 
 if __name__ == '__main__':
--- web/users/routes.py
+++ web/users/routes.py
@@ -1,3 +1,17 @@
 from flask import render_template, Blueprint
 
-users = Blueprint('base', __name__)
+from web.users.forms import RegistrationForm, LoginForm
+
+users = Blueprint('users', __name__)
+
+
+@users.route('/registration', methods=['GET', 'POST'])
+def registration():
+    registration_form = RegistrationForm()
+    return render_template('registration.html', form=registration_form)
+
+
+@users.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
