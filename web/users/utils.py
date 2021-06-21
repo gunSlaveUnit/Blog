634ce270a3125cf7788d656bbbd6a012b2a9cc9d
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-06-21 21:27:39 +0300

    Added profile image resizing

--- web/users/utils.py
+++ web/users/utils.py
@@ -1,6 +1,8 @@
 import os.path
 import secrets
 
+from PIL import Image
+
 from manage import app
 
 
@@ -9,5 +11,10 @@ def save_user_account_image(image):
     _, extension = os.path.splitext(image.filename)
     image_filename = random_hex + extension
     image_path = os.path.join(app.root_path, 'static/media/users', image_filename)
-    image.save(image_path)
+
+    output_size = (256, 256)
+    i = Image.open(image)
+    i.thumbnail(output_size)
+
+    i.save(image_path)
     return image_filename
