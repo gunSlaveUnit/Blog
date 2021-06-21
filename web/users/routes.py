ab164163479d284850950418b241d3c45688dc47
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-06-21 18:16:50 +0300

    Added routes: logout and account. Added account page

--- web/users/routes.py
+++ web/users/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, Blueprint, flash, redirect, url_for
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user
 
 from manage import bcrypt, db
 from web.users.forms import RegistrationForm, LoginForm
@@ -39,3 +39,14 @@ def login():
         else:
             flash('Login unsuccessful. Check email and password', 'danger')
     return render_template('login.html', form=login_form)
+
+
+@users.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('base.home'))
+
+
+@users.route('/account')
+def account():
+    return render_template('account.html', title='Account')
