35591ecfba88d672a814dc497263c81ab4d51743
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-06-29 14:34:13 +0300

    Trying to add cache

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ config = dotenv_values('.env')
 class Config(object):
     SECRET_KEY = config["SECRET_KEY"]
     SQLALCHEMY_DATABASE_URI = config["SQLALCHEMY_DATABASE_URI"]
+    SQLALCHEMY_TRACK_MODIFICATIONS = config["SQLALCHEMY_TRACK_MODIFICATIONS"]
     MAIL_SERVER = config["MAIL_SERVER"]
     MAIL_PORT = config["MAIL_PORT"]
     MAIL_USE_TLS = config["MAIL_USE_TLS"]
--- web/base/routes.py
+++ web/base/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, Blueprint, request
 
+from services import cache
 from web.posts.models import Post
 
 base = Blueprint('base', __name__)
@@ -8,5 +9,8 @@ base = Blueprint('base', __name__)
 @base.route('/')
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date.desc()).paginate(page=page, per_page=5)
+    posts = cache.get(f'posts_{page}')
+    if posts is None:
+        posts = Post.query.order_by(Post.date.desc()).paginate(page=page, per_page=5)
+        cache.set(f'posts_{page}', posts)
     return render_template('home.html', title='Home', posts=posts)
